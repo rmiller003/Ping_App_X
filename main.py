6f1e610f07a90d80adeeb9e44182ad037f045052
robert miller <robert.miller@loblaw.ca>
2023-04-16 21:27:11 -0400

    Stage 2

--- main.py
+++ main.py
@@ -4,7 +4,16 @@ window = Tk()
 window.wm_geometry("400x50")
 window.title("Tkinter App X")
 
-lbl = Label(window, text="Hello World")
-lbl.pack()
+lbl_1 = Label(window, text="Default Formatting")
+lbl_1.pack()
+
+lbl_2 = Label(window, text="Red Text", fg="red")
+lbl_2.pack()
+
+lbl_3 = Label(window, text="RAISED BLUE Text", fg="blue", relief="raised")
+lbl_3.pack()
+
+lbl_4 = Label(window, text="Text with Yellow Background", bg="yellow")
+lbl_4.pack()
 
 window.mainloop()
